0c369a3587b4c23c0e92d8ab71b4d11593439f01
Thomas Banks <thomas.banks@sky.uk>
2023-12-03 11:04:31 +0000

    Day 1 - challenge 1

---
+++ crates/day-01/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+/**
+ * First result
+ */
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let result: u32 = io::BufReader::new(file)
+        .lines()
+        .into_iter()
+        .map(|line| {
+            line.unwrap()
+                .chars()
+                .filter(|char| char.is_numeric())
+                .map(|char| {
+                    char.to_digit(10).unwrap()
+                })
+                .collect::<Vec<u32>>()
+        })
+        .map(|nums| {
+            let first = nums.first().unwrap();
+            let last = nums.last().unwrap();
+
+            return (first * 10) + last;
+        })
+        .sum();
+
+    println!("{}", result);
+    Ok(())
+}
